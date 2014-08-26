f61fa31df7d308e2d4bec3a71d5b763cba62c4e7
luanpm88 <luanpm88@gmail.com>
2014-08-26 10:52:57 +0700

    change access token fb

--- fb/test.php
+++ fb/test.php
@@ -3,14 +3,14 @@
 require_once("sdk/facebook.php"); // set the right path
     echo "1";
 $config = array();
-$config['appId'] = '1474589189465986';
-$config['secret'] = 'f02d8dc5fcb156d9cb393f26db039d25';
+$config['appId'] = '632064076907388';
+$config['secret'] = 'ad9ff0da7689970bff20c31ac770f90c';
 $config['fileUpload'] = false; // optional
 $fb = new Facebook($config);
     echo "2";
 $params = array (
     //sthis is the main access token (facebook profile)
-    "access_token" => "CAAU9IYzT54IBALNhrf5UxjQOtujID7UbCdCOHyRfidWT1LZBYwiZAwOUCDlGQJPJR8gsMZB3ovSZCZAOyC1TGR55Ssl5ltKW2EQNvEIgI6BnZB1Tz1vPhi5rNZB5GusM78i7vMcJw2FSOYtvCrGxXrQPftDPx9pmW07EvZBBjuJGTZCmIKsqk0YMLrEfVBAMKpv2SFUzVH6WqxpQyBRKWjdaK",
+    "access_token" => "CAAIZB2ZBLHg3wBAC0gAO4T6zZAXjFrcEpwEgyaZAgdYIuJlkAKANZCIMHhvuUIwEkvzwoKZBfLNVGKrNnBRAURBUccbnlWimiPD1gDjVZCqyybnxpQAR8FwH4oYz5ZAB7EJuJHZCUvQK68y79hTm8GsRDHNCZBxP0y7uKTibX5QhjZAOy50N4ilOeMRZBLx26tBZClxAepxeOG07ST0YX104C1K8ZA",
     "message" => "Here is a blog post about auto posting on Facebook using PHP #php #facebook",
     "link" => "http://www.pontikis.net/blog/auto_post_on_facebook_with_php",
     "picture" => "http://i.imgur.com/lHkOsiH.png",
