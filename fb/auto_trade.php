2af1a4f80a98c61654882254a8ed1e8a4bd8f5b3
luanpm88 <luanpm88@gmail.com>
2014-08-27 10:20:34 +0700

    update fb share product

--- fb/auto_trade.php
+++ fb/auto_trade.php
@@ -97,15 +97,14 @@ foreach($share_topics as $share_topic) {
  
     sleep(3);
   }
- 
 }
  
 if(php_sapi_name() == 'cli') {
   // keep log
   file_put_contents('/home/marketon/domains/marketonline.vn/public_html/fb/auto_trade.log', $result . str_repeat('=', 80) . PHP_EOL, FILE_APPEND);
- 
+
   echo $result;
- 
+  
 } else {
   $html = '<html><head>';
   $html .= '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">';
