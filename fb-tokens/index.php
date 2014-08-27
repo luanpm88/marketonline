7fa24225567e35f3c0b093708670856f610eb1d0
luanpm88 <luanpm88@gmail.com>
2014-08-27 16:44:55 +0700

    update fb share product

--- fb-tokens/index.php
+++ fb-tokens/index.php
@@ -15,11 +15,14 @@
         $member->saveField("fb_code", $_GET["code"], intval($user["id"]));
         
         $access_string =  file_get_contents("https://graph.facebook.com/oauth/access_token?client_id=".$admin["fb_app_id"]."&redirect_uri=http://marketonline.vn/fb-tokens/&client_secret=".$admin["fb_secret"]."&code=".$_GET["code"]);
+        echo $access_string;
         $access_string = explode("=",$access_string);
         $access_token = $access_string[1];
         
         $access_token = explode("&",$access_token);
         $access_token = $access_token[0];
+        
+        echo $access_token;
         
         $member->saveField("fb_access_token", $access_token, intval($user["id"]));
         
@@ -27,7 +30,9 @@
         
         $member->saveField("fb_data", $data, intval($user["id"]));
         
-        pheader("location: ../virtual-office/company.php");
+        echo $data;
+        
+        //pheader("location: ../virtual-office/company.php");
     }
 
 ?>
