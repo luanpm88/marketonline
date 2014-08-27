567c8b6e261ed607da6407f52fe4c3efdb66ce69
luanpm88 <luanpm88@gmail.com>
2014-08-27 13:12:03 +0700

    update fb share product

--- fb-tokens/index.php
+++ fb-tokens/index.php
@@ -1,3 +1,23 @@
 <?php
-    print_r($_GET);
+    //print_r($_GET);
+    define('CURSCRIPT', 'index');
+    //$cache_archiver = true;
+    $htmlize = true;
+    require("../libraries/common.inc.php");
+    require("../share.inc.php");
+
+    $member = new Members();
+    $pb_userinfo = pb_get_member_info();
+    $user = $member->read("*", $pb_userinfo["pb_userid"]);
+    
+    if(isset($_GET["code"])) {
+        $member->saveField("fb_code", $_GET["code"], intval($user["id"]));
+        
+        $access_string =  file_get_contents("https://graph.facebook.com/oauth/access_token?client_id=".$user["fb_app_id"]."&redirect_uri=http://marketonline.vn/fb-tokens/&client_secret=".$user["fb_secret"]."&code=".$_GET["code"]);
+        $access_string = explode("=",$access_string);
+        $access_token = $access_string[1];
+        
+        $member->saveField("fb_access_token", $access_token, intval($user["id"]));        
+    }
+
 ?>
