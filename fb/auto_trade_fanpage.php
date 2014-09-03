fb294118a17324f1874971283d4d7c417246241f
luanpm88 <luanpm88@gmail.com>
2014-09-03 11:20:51 +0700

    update fb fanpage

--- fb/auto_trade_fanpage.php
+++ fb/auto_trade_fanpage.php
@@ -7,11 +7,11 @@ require_once('sharelib.php');
 
 
 // create array with topics to be posted on Facebook
-$sql = 'SELECT type.name as type_name, com.cache_spacename, com.name as company_name, trade.id, trade.facebook_pubstatus, trade.title, trade.content, trade.picture, trade.picture1, trade.picture2, trade.picture3, trade.picture4'    
+$sql = 'SELECT type.name as type_name, com.cache_spacename, com.name as company_name, trade.id, trade.facebook_pubstatus_fanpage, trade.title, trade.content, trade.picture, trade.picture1, trade.picture2, trade.picture3, trade.picture4'    
     .' FROM pb_trades trade'
     .' LEFT JOIN pb_companies as com ON com.id = trade.company_id'
     .' LEFT JOIN pb_tradetypes as type ON type.id = trade.type_id'
-    .' WHERE facebook_pubstatus=0'
+    .' WHERE facebook_pubstatus_fanpage=0'
     .' LIMIT 1';
 
 $rs = $conn->query($sql);
@@ -24,7 +24,7 @@ $share_topics = array();
 while($res_s = $rs->fetch_assoc()) {
     //prepair content
     $res["id"] = $res_s["id"];
-    $res["facebook_pubstatus"] = $res_s["facebook_pubstatus"];
+    $res["facebook_pubstatus_fanpage"] = $res_s["facebook_pubstatus_fanpage"];
     $res['url'] = "http://marketonline.vn/thuong-mai/".$res_s['id']."/".stringToURI($res_s['title']);    
     $res["title"]= str_replace('[:vi-vn]', '', $res_s["title"]);
     $res["content"]= substr(strip_tags(str_replace('[:vi-vn]', '', $res_s["content"])),0,9000);
@@ -64,7 +64,7 @@ $result = '';
 // AUTOMATIC POST EACH TOPIC TO FACEBOOK
 foreach($share_topics as $share_topic) {
  
-  if($share_topic['facebook_pubstatus'] == 0) {
+  if($share_topic['facebook_pubstatus_fanpage'] == 0) {
   //if(true) {  
     // define POST parameters
     $params = array(
@@ -82,22 +82,22 @@ foreach($share_topics as $share_topic) {
  
     // check if topic successfully posted to Facebook
     try {
-      $ret = $fb->api('/me/feed', 'POST', $params); // configure appropriately
+      //$ret = $fb->api('/me/feed', 'POST', $params); // configure appropriately
       
-//      //for fan page
-//	$fb_data = json_decode($admin["fb_data"], true);
-//	foreach($fb_data["data"] as $fb_fanpage) {
-//		$params["access_token"] = $fb_fanpage["access_token"];
-//		$fb->api('/'.$fb_fanpage["id"].'/feed', 'POST', $params);
-//		$result .= ' SUCCESSFUL... (Posted to ['.$fb_fanpage["name"].'] Fanpage) : ' . $share_topic['title'] . ' - ' . $share_topic['url'] . $line_break;
-//	}
+      //for fan page
+	$fb_data = json_decode($admin["fb_data"], true);
+	foreach($fb_data["data"] as $fb_fanpage) {
+		$params["access_token"] = $fb_fanpage["access_token"];
+		$fb->api('/'.$fb_fanpage["id"].'/feed', 'POST', $params);
+		$result .= ' SUCCESSFUL... (Posted to ['.$fb_fanpage["name"].'] Fanpage) : ' . $share_topic['title'] . ' - ' . $share_topic['url'] . $line_break;
+	}
       
       // mark topic as posted (ensure that it will be posted only once)
-      $sql = 'UPDATE pb_trades SET facebook_pubstatus = 1 WHERE id = ' . $share_topic['id'];
+      $sql = 'UPDATE pb_trades SET facebook_pubstatus_fanpage = 1 WHERE id = ' . $share_topic['id'];
       if($conn->query($sql) === false) {
         trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
       }
-      $result .= ' SUCCESSFUL... (Posted to [ME] Wall) : ' . $share_topic['title'] . ' - ' . $share_topic['url'] . $line_break;
+      //$result .= ' SUCCESSFUL... (Posted to [ME] Wall) : ' . $share_topic['title'] . ' - ' . $share_topic['url'] . $line_break;
  
     } catch(Exception $e) {
       $result .= ' FAILED... (' . $e->getMessage() . ') : ' . $share_topic['title'] . ' - ' . $share_topic['url'] . $line_break;
@@ -112,7 +112,7 @@ if($result) $result .= date("Y-m-d H:i:s") . $line_break;
 
 if(php_sapi_name() == 'cli') {
   // keep log
-  if($result) file_put_contents('/home/marketon/domains/marketonline.vn/public_html/fb/auto_trade.log', $result . str_repeat('=', 80) . PHP_EOL, FILE_APPEND);
+  if($result) file_put_contents('/home/marketon/domains/marketonline.vn/public_html/fb/auto_trade_fanpage.log', $result . str_repeat('=', 80) . PHP_EOL, FILE_APPEND);
 
   echo $result;
   
