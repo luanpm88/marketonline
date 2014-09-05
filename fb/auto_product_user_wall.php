e21e33cc6da4e9ea1572eb0ff172da5487e5b9a4
luanpm88 <luanpm88@gmail.com>
2014-09-05 12:03:08 +0700

    update facebook

--- fb/auto_product_user_wall.php
+++ fb/auto_product_user_wall.php
@@ -78,7 +78,8 @@ foreach($share_topics as $share_topic) {
     // define POST parameters
     $params = array(
       "access_token" => $share_topic["fb_access_token"], // configure appropriately
-      "message" => $share_topic['post_title'],
+      //"message" => $share_topic['post_title'],
+      "message" => "",
       "link" => $share_topic['url'],
       "name" => $share_topic['title'],
       "caption" => "http://marketonline.vn", // configure appropriately
--- fb/auto_trade_user_fanpage.php
+++ fb/auto_trade_user_fanpage.php
@@ -75,7 +75,8 @@ foreach($share_topics as $share_topic) {
  
   $params = array(
     "access_token" => $share_topic["fb_access_token"], // configure appropriately
-    "message" => $share_topic['post_title'],
+    //"message" => $share_topic['post_title'],
+    "message" => "",
     "link" => $share_topic['url'],
     "name" => $share_topic['title'],
     "caption" => "http://marketonline.vn", // configure appropriately
