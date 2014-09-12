ead4b8e3cc8c1664cdb23ac18af706857c05759f
luanpm88 <luanpm88@gmail.com>
2014-09-12 12:10:39 +0700

    update ctv

--- libraries/core/controllers/product_controller.php
+++ libraries/core/controllers/product_controller.php
@@ -728,7 +728,7 @@ class Product extends PbController {
 		if(empty($info) || !$info || $info["valid_status"] != 1) {
 			//$pb_userinfo = pb_get_member_info();
 			//$member_info = $member->getInfoById($pb_userinfo['pb_userid']);
-			if($permissions["valid"] || ($permissions["unvalid"] && $info["valid_moderator"] == $pb_userinfo["pb_userid"])) {
+			if($permissions["valid"] || ($permissions["unvalid"])) {
 				if($info["valid_status"] == 0) {
 					setvar("pending","<span class='unvalid'>Không hợp lệ (".$info["valid_message"].")</span>");
 				} elseif ($info["valid_status"] == 3) {
