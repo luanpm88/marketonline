4b7c44d3a5a0d3402f950ac5d257c6f9e4a16ee9
luanpm88 <luanpm88@gmail.com>
2014-09-17 08:53:46 +0700

    commit

--- fb/auto_point.php
+++ fb/auto_point.php
@@ -10,6 +10,8 @@ $pointdb = new Points();
 
 $joins = array();
 
+$result = "";
+
 $conditions = array();
 $joins = array();
 $joins[] = "LEFT JOIN (SELECT member_id, MAX(created) AS max_date FROM {$memberdb->table_prefix}pointlogs pointlogs WHERE pointlogs.action_name='connect_facebook' GROUP BY member_id) logs ON logs.member_id=Member.id";
@@ -23,6 +25,8 @@ foreach($rows as $member) {
         $pointdb->update("connect_facebook",intval($member["id"]));
     }
 }
+if(count($rows)) $result .= "Num of connect_facebook:" . count($rows) . $line_break;
+
 
 $conditions = array();
 $joins = array();
@@ -34,6 +38,30 @@ $rows = $memberdb->findAll("logs.max_date, Member.*", $joins, $conditions);
 
 foreach($rows as $member) {    
     $pointdb->update("checkout",intval($member["id"]));
+}
+if(count($rows)) $result .= "Num of checkout:" . count($rows) . $line_break;
+
+
+//write log
+if($result) $result .= date("Y-m-d H:i:s") . $line_break;
+
+
+if(php_sapi_name() == 'cli') {
+  // keep log
+  if($result) file_put_contents('/home/marketon/domains/marketonline.vn/public_html/fb/auto_product.log', $result . str_repeat('-', 80) . PHP_EOL, FILE_APPEND);
+ 
+  echo $result;
+ 
+} else {
+  $html = '<html><head>';
+  $html .= '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">';
+  $html .= '</head>';
+  $html .= '<body>';
+  $html .= $result;
+  $html .= '</body>';
+  $html .= '</html>';
+  
+  echo $html;
 }
 
 ?>
