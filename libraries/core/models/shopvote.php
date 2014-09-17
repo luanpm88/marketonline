7d9efbaec936b058a5048d70fa7a380c188e7f51
luanpm88 <luanpm88@gmail.com>
2014-09-17 16:53:01 +0700

    commit

---
+++ libraries/core/models/shopvote.php
@@ -0,0 +1,72 @@
+<?php
+class Shopvotes extends PbModel {
+ 	var $name = "Shopvote";
+
+ 	function Shopvotes()
+ 	{
+ 		parent::__construct();
+ 	}
+ 	
+ 	function update($member_id, $space_name, $star)
+ 	{
+ 		uses('company');
+		$companydb = new Companies();
+		
+		$company_id = $companydb->field("id",array("LOWER(cache_spacename)='".strtolower($space_name)."'"));
+		if($company_id && $star <= 5) {
+			$exsit = $this->fields("*",array("member_id=".$member_id,"company_id=".$company_id));
+			
+			if($exsit) {
+				if($star == 0) {
+					$this->del(intval($exsit["id"]));
+				} else {
+					$exsit["rate"] = $star;
+					$exsit["created"] = date('Y-m-d H:i:s');
+					
+					//var_dump($exsit);
+					
+					$this->save($exsit,'update',intval($exsit["id"]));
+				}
+			} else {
+				$vals["rate"] = $star;
+				$vals["member_id"] = $member_id;
+				$vals["company_id"] = $company_id;
+				$vals["created"] = date('Y-m-d H:i:s');
+				
+				$this->save($vals);
+			}
+		}
+		
+		//UPDATE SHOP RATE
+		$new_rate = $this->getResult($company_id);
+		$fields["vote_count"] = $new_rate["count"];
+		$fields["vote_sum"] = $new_rate["sum"];
+		$fields["vote_result"] = $new_rate["result"];
+		
+		$companydb->save($fields,'update',intval($company_id));
+ 	}
+	
+	function getVote($member_id,$company_id) {
+		//echo $member_id.$company_id;
+		$exsit = $this->fields("*",array("member_id=".$member_id,"company_id=".$company_id));
+		if($exsit) {
+			return $exsit;
+		} else {
+			return false;
+		}
+	}
+ 	
+	function getResult($company_id)
+	{
+		$sum = $this->fields("sum(rate) as sum",array("company_id=".$company_id));
+		$sum = $sum["sum"];
+		$count = $this->fields("count(id) as total",array("company_id=".$company_id));
+		$count = $count["total"];
+		
+		$result = 0;
+		if($count) $result = $sum/$count;
+		
+		return array("count"=>$count,"sum"=>$sum,"result"=>$result);
+	}
+}
+?>
