c8ddb7936c0b088f1de9049f633ebea157684a94
luanpm88 <luanpm88@gmail.com>
2014-09-25 13:41:03 +0700

    commit

--- libraries/core/models/album.php
+++ libraries/core/models/album.php
@@ -9,17 +9,8 @@ class Albums extends PbModel {
 	
 	function screenshot($file) {
 		echo $file;
-		//$dir = "/home/marketon/domains/marketonline";
-		exec("ls", $out, $err);
-		//echo "ffmpeg -i ../../../".$file." -ss 00:00:5.435 -f image2 -vframes 1 ../../../".$file.".thumb.png";
-		//exec("ls /", $out, $err);
-		var_dump($out);
-		var_dump($err);
-		exec("ffmpeg -i ../../../".$file." -ss 00:00:5.435 -f image2 -vframes 1 ../../../".$file.".thumb.png", $out, $err);
+		echo shell_exec("ffmpeg -i ../../../".$file." -ss 00:00:5.435 -f image2 -vframes 1 ../../../".$file.".thumb.png");
 		echo "ffmpeg -i ../../../".$file." -ss 00:00:5.435 -f image2 -vframes 1 ../../../".$file.".thumb.png";
-		//exec("ls /", $out, $err);
-		var_dump($out);
-		var_dump($err);
 	}
 }
 ?>
