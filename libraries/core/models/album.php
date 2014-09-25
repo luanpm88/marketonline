acd30eee1b72e91bbe4dbc979cdb3d7a5ebb9025
luanpm88 <luanpm88@gmail.com>
2014-09-25 14:42:41 +0700

    commit

--- libraries/core/models/album.php
+++ libraries/core/models/album.php
@@ -8,28 +8,8 @@ class Albums extends PbModel {
  	}
 	
 	function screenshot($file) {
-		$file = "/home/marketon/domains/marketonline.vn/public_html/".$file;
-		
-		$time =  exec("ffmpeg -i {$file} 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//");   
-
-		// duration in seconds; half the duration = middle
-		$duration = explode(":",$time);   
-		$durationInSeconds = $duration[0]*3600 + $duration[1]*60+ round($duration[2]);
-		$durationMiddle = $durationInSeconds/2;
-		
-		// recalculte to minutes and seconds
-		$minutes = $durationMiddle/60;
-		$realMinutes = floor($minutes);
-		$realSeconds = round(($minutes-$real_minutes)*60);
-		
-		var_dump($realMinutes);
-		var_dump($realSeconds);
-		
-		
 		//exec("ffmpeg -i album-757-118.mp4 -ss 00:00:5.435 -f image2 -vframes 1 album-757-118.mp4.thumb.png",$output,$vars);
-		
-		exec("ffmpeg -i {$file} -ss {$realMinutes}:{$realSeconds}:1.0 -f image2 -vframes 1 {$file}.thumb.png",$output,$vars);
-		echo "ffmpeg -i {$file} -ss {$realMinutes}:{$realSeconds}:1.0 -f image2 -vframes 1 {$file}.thumb.png";
+		exec("ffmpeg -i /home/marketon/domains/marketonline.vn/public_html/{$file} -ss 00:00:5.435 -f image2 -vframes 1 /home/marketon/domains/marketonline.vn/public_html/{$file}.thumb.png",$output,$vars);
 		var_dump($output);
 		var_dump($vars);
 		
