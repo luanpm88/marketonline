7899cc79df05cc25529efb9a6219ed920cebce02
luanpm88 <luanpm88@gmail.com>
2014-09-25 13:45:27 +0700

    commit

--- libraries/core/models/album.php
+++ libraries/core/models/album.php
@@ -9,7 +9,7 @@ class Albums extends PbModel {
 	
 	function screenshot($file) {
 		echo $file;
-		var_dump(shell_exec("ls"));
+		var_dump(shell_exec("ffmpeg"));
 		echo "ffmpeg -i ../../../".$file." -ss 00:00:5.435 -f image2 -vframes 1 ../../../".$file.".thumb.png";
 	}
 }
