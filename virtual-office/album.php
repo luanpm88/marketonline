dc2bb2a0f5403af195a9c61cfa48f19d286150d5
luanpm88 <luanpm88@gmail.com>
2014-09-25 16:14:37 +0700

    commit

--- virtual-office/album.php
+++ virtual-office/album.php
@@ -135,12 +135,15 @@ if(isset($_GET["type"])) {
 $amount = $album->findCount($joins, $conditions, "Album.id");
 $page->setPagenav($amount);
 $res = $pdb->GetAll("SELECT * from {$tb_prefix}albums");
-$result = $album->findAll("Album.attachment_id,Album.type,Attachment.title,Attachment.modified,Attachment.description,Attachment.attachment,Album.id", $joins, $conditions, "Album.id DESC", $page->firstcount, $page->displaypg);
+$result = $album->findAll("Album.album_id,Album.attachment_id,Album.type,Attachment.title,Attachment.modified,Attachment.description,Attachment.attachment,Album.id", $joins, $conditions, "Album.id DESC", $page->firstcount, $page->displaypg);
 if (!empty($result)) {
 	for($i=0; $i<count($result); $i++){
 		$result[$i]['image'] = pb_get_attachmenturl($result[$i]['attachment'], '../', "small");
 		if($result[$i]['type']=='video') {
 			$result[$i]['image'] = URL."attachment/".$result[$i]['attachment'].".thumb.png?v=".$result[$i]['modified'];
+		}
+		if($result[$i]['album_id']) {
+			$result[$i]['image'] = URL."attachment/".$result[$i]['attachment'].".thumb.jpg?v=".$result[$i]['modified'];
 		}
 	}
 	setvar("Items", $result);
