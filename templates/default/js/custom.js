a69ced7117cdc2686e48167def9096560f116526
luanpm88 <luanpm88@gmail.com>
2014-10-31 10:07:47 +0700

    commit

--- templates/default/js/custom.js
+++ templates/default/js/custom.js
@@ -2527,8 +2527,8 @@
 		$('.video-overlay').append('<span class="vv-oo"></span>');
 		$('.album_a_block').hover(function(){$(this).parent().find('.vv-oo').addClass('active')},function(){$(this).parent().find('.vv-oo').removeClass('active')});
 		$('.company-container .album.video_new_page .filterable-grid .pic img').resizecrop({
-			width:219,
-			height:123		   
+			width:221,
+			height:124		   
 		});
 		$('.company-container .album.album_new_page .filterable-grid .pic img').resizecrop({
 			width:219,
