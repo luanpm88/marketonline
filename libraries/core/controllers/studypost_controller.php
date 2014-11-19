868be3b52db7fd09644f05d6b0711878a282b373
luanpm88 <luanpm88@gmail.com>
2014-11-19 12:35:41 -0800

    commit

--- libraries/core/controllers/studypost_controller.php
+++ libraries/core/controllers/studypost_controller.php
@@ -653,7 +653,7 @@ class Studypost extends PbController {
 		setvar("school_list", $school_list);
 		setvar("member", $member);
 		
-		render("modern/studypost/memberpage");
+		render("studypost/memberpage");
 	}
 	
 	function ajaxFollow()
