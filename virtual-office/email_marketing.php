cfb19154d285e507a1f1b8b43f8c60e7222afa8e
luanpm88 <luanpm88@gmail.com>
2014-12-23 10:38:27 +0700

    commit

---
+++ virtual-office/email_marketing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *      [PHPB2B] Copyright (C) 2007-2099, Ualink Inc. All Rights Reserved.
+ *      The contents of this file are subject to the License; you may not use this file except in compliance with the License. 
+ *
+ *      @version $Revision: 2238 $
+ */
+require("../libraries/common.inc.php");
+require("room.share.php");
+require(LIB_PATH. 'page.class.php');
+require(CACHE_COMMON_PATH."cache_type.php");
+$tpl_file = "email_marketing";
+
+template($tpl_file);
+?>
