d374e0c99243d847bacfadd6695de72cd7119b39
luanpm88 <luanpm88@gmail.com>
2015-01-30 14:42:37 +0700

    commit

---
+++ space/service.inc.php
@@ -0,0 +1,11 @@
+<?php
+if(!defined('IN_PHPB2B')) exit('Not A Valid Entry Point');
+setvar("service_page",1);
+setvar("pagetitle","Dịch vụ");
+if(detectMobile()) {
+	$space->render_mobile("space/product");
+} else {
+	pheader("location: /".$member->info['space_name']);
+}
+
+?>
