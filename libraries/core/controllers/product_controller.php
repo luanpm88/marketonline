95dd890913f74067df4c31e04ef2f3654725d449
Luan Pham <luanpm88@gmail.com>
2015-12-06 19:14:33 -0800

    update

--- libraries/core/controllers/product_controller.php
+++ libraries/core/controllers/product_controller.php
@@ -1407,12 +1407,14 @@ class Product extends PbController {
 								$level = $item["level"];
 								
 								setvar('current_cat', $item);
+								echo "ssssss"
 							}
 							if($_GET["memberid"] != '' && isset($item["member_id"]))
 							{
 								$custom_array[] = $id_i;
 								$level = $item["level"];
 								setvar('current_cat', $item);
+								echo "ssssss222222"
 							}
 							
 						}
