3f453d16e517a9155012fbaa7b584bbb3dcff66c
Luan Pham <luanpm88@gmail.com>
2015-12-14 02:01:30 -0800

    market pages

--- libraries/core/controllers/area_controller.php
+++ libraries/core/controllers/area_controller.php
@@ -376,6 +376,13 @@ class Area extends PbController {
 			$offset = $row*$num*($_GET["p"]-1);			
 		}
 		
+		
+		
+		setvar("areatype", $areatype);
+		setvar("area", $area);
+		setvar("industry", $industry);
+		setvar("industries_list", $industries_list);
+		$trades = $this->trade->getByArea(array("industries"=>$industries,"area_id"=>$area_id,"areatype_id"=>$areatype_id,"type_id"=>$type_id),$offset,$row,$num);
 		$products = $trades["result"]
 		foreach($products as $key => $item)
 		{
@@ -408,12 +415,6 @@ class Area extends PbController {
 			if($aaaa[1]) $products[$key]['area_names'] = $aaaa[1];
 			
 		}
-		
-		setvar("areatype", $areatype);
-		setvar("area", $area);
-		setvar("industry", $industry);
-		setvar("industries_list", $industries_list);
-		$trades = $this->trade->getByArea(array("industries"=>$industries,"area_id"=>$area_id,"areatype_id"=>$areatype_id,"type_id"=>$type_id),$offset,$row,$num);
 		setvar("trades",$products);
 		setvar("count",$trades["count"]);
 		
