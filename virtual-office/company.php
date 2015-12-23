635659b3df023d565736e8a34b8d321d16d9d77e
Luan Pham <luanpm88@gmail.com>
2015-12-23 00:45:12 -0800

    update

--- virtual-office/company.php
+++ virtual-office/company.php
@@ -445,7 +445,7 @@ setvar("PageTitle","Hồ sơ");
 
 $adm = $member->read("*", 1);
 setvar("fb_app_id", $adm["fb_app_id"]);
-var_dump($adm["fb_app_id"]);
+//var_dump($adm["fb_app_id"]);
 
 
 if(detectMobile()) {	
