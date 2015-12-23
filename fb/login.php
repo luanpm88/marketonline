346c614e95f27c6c537418d14a92fc2bfa2c013d
Luan Pham <luanpm88@gmail.com>
2015-12-22 18:15:41 -0800

    facebook sdk

--- fb/login.php
+++ fb/login.php
@@ -1,10 +1,10 @@
 <?php
-echo "sss";
+
 # login.php
 require_once("sharelib_v4.php");
 
 $helper = $fb->getRedirectLoginHelper();
-$permissions = ['email','publish_actions','publish_pages']; // optional
+$permissions = array('email','publish_actions','publish_pages'); // optional
 $loginUrl = $helper->getLoginUrl('http://test.marketonline.vn/fb/login-callback.php', $permissions);
 
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
