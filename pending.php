0acd7b886803adcfca9ef641d9bc14637174ecc3
Luan Pham <luanpm88@gmail.com>
2016-01-13 00:19:40 -0800

    update

--- pending.php
+++ pending.php
@@ -33,7 +33,7 @@ if (!empty($validate_str)) {
 			$member->logOut();
     			$result = $member->updateUserStatus($member->info['id']);
     			if ($result) {
-					if(isset($_GET["new_user_id"])) {
+					if(isset($_GET["new_user_id"]) && $_GET["new_user_id"] != "") {
 						flash(array("message"=>'Bạn hãy <a href="logging.php?type=new"><strong>&nbsp;&nbsp;Đăng nhập&nbsp;&nbsp;</strong></a> để theo dõi đơn hàng và hưởng chính sách ưu đãi.<br /><br />Bạn có thể dùng tài khoản này để kinh doanh và hoạt động trên MarketOnline.vn', "box_title"=>"Đăng ký tài khoản thành công", "page_title"=>"Đăng ký"), "logging.php?type=new");
 					} else {
 						flash(array("message"=>"actived_and_login", "box_title"=>"Đăng ký thành công", "page_title"=>"Đăng ký"), "logging.php?type=new");
