ee2957b031249965a0de8844c662a5d34909aae3
Luan Pham <luanpm88@gmail.com>
2016-06-14 21:10:06 -0700

    remove link product content

--- libraries/core/controllers/product_controller.php
+++ libraries/core/controllers/product_controller.php
@@ -930,14 +930,14 @@ class Product extends PbController {
 		// $info['content'] = preg_replace('/\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|$!:,.;]*[A-Z0-9+&@#\/%=~_|$]/i', '', $info['content']);
 		
 		
-//		// remove domain
-//		$info['content'] = str_replace('marketonline.vn', '$$$$$$kkkkkk$$$$$$', $info['content']);
-//		$info['content'] = preg_replace('|https?://www\.[a-z\.0-9]+|i', '', $info['content']);
-//        $info['content'] = preg_replace('|www\.(?!marketonline.vn)[a-z\.0-9]+|i', '', $info['content']);
-//        $info['content'] = preg_replace('/[a-z\.0-9]+\.(vn|com|org|net|se|edu|com\.vn)/i', '', $info['content']);
-//		$info['content'] = str_replace('$$$$$$kkkkkk$$$$$$', 'marketonline.vn', $info['content']);
-//		// remove phone number
-//		$info['content'] = preg_replace('/([0-9]+[\- \.]*){9,}/','',$info['content']);
+		// remove domain
+		$info['content'] = str_replace('marketonline.vn', '$$$$$$kkkkkk$$$$$$', $info['content']);
+		$info['content'] = preg_replace('|https?://www\.[a-z\.0-9]+|i', '', $info['content']);
+        $info['content'] = preg_replace('|www\.(?!marketonline.vn)[a-z\.0-9]+|i', '', $info['content']);
+        $info['content'] = preg_replace('/[a-z\.0-9]+\.(vn|com|org|net|se|edu|com\.vn)/i', '', $info['content']);
+		$info['content'] = str_replace('$$$$$$kkkkkk$$$$$$', 'marketonline.vn', $info['content']);
+		// remove phone number
+		// $info['content'] = preg_replace('/([0-9]+[\- \.]*){9,}/','',$info['content']);
 		
 		
 		//format html
